800b69852e98e7a6f594bd86f00a2a1f72015653
Divyansh Singh <singh.divyanshpbh29@gmail.com>
2026-06-06 21:05:51 +0530

    Day 9 Problem Done

---
+++ Day_9/problem3.cpp
@@ -0,0 +1,25 @@
+// Write a program to Print repeated character pattern.
+// A
+// BB
+// CCC
+// DDDD
+// EEEEE
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        char ch = 'A' + i - 1; 
+        for (int j = 1; j <= i; j++) {
+            cout << ch;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
